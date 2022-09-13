4fc1d4e7f5c3eef068e282b77254d792fe9b4751
ahuntercatalano <alex.catalano2@gmail.com>
2022-09-13 16:44:11 -0400

    colors

---
+++ colors/index.php
@@ -0,0 +1,58 @@
+#663200 brown
+#835300 brown_light
+#c2520a orange_dark
+#dc661e orange_medium
+#f45a1e orange
+#f37020 orange_light_1
+#f6821f orange_light_2
+#ff9f0d orange_light_3
+#ff9f0d yellow
+
+#012346 blue_dark_3
+#01337a blue_dark_2
+#014083 blue_dark_1
+#00529b blue_medium
+#1670d0 blue
+#347ab6 blue_light
+#00a4a6 green_dark
+#aadcdb green_light
+#b9e986 green_medium
+
+#414141 gray_dark
+#685546 brown_dark
+
+
+85546 brown_dar
+
+
+
+git branch -c <feature>
+  git checkout <feature>
+
+
+
+    [4:28 PM] Williams, Brent
+    git push -u <feature> <feature>
+
+
+                                 ~/acatalano2
+
+                                 - local machine
+
+                                     push to
+
+                                 - test-tools  (github / alex)
+
+
+                                - ls-web-mgmt/test-tools (root / ssh / command prompt)
+
+                                - rsync -avr ./tools /web-share-nfs/content/ --dry-run
+
+
+                                  (World Wide Web)
+
+
+
+
+
+  rsync -avr /web-share-nfs/content/sistes/alex/* /home/acatalano2/test-tools/tools/
